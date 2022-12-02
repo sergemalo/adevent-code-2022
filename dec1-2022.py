3faf71b7cc662551d078f8d107c42a70d7d22d2d
Serge Malo <sergemalo2000@outlook.com>
2022-12-02 10:49:05 -0500

    Completed Dec 1 Part 2

--- dec1-2022.py
+++ dec1-2022.py
@@ -1,24 +1,56 @@
-import sys
+import copy
+
+class Elf:
+    def __init__(self):
+        self.snacks = []
+
+    def calories(self):
+        totalCalories = 0
+        for c in self.snacks:
+            totalCalories = totalCalories + c
+        return totalCalories
 
 f = open("dec1-dataset.txt", "rt")
 lineCount = 0
-elfCount  = 0
-curFood = 0
-maxFood = 0
+elfCount = 0
+curElf = Elf()
+curElfTotalCal = 0
+maxCalForOneElf = 0
+
+elfList = []
 for line in f:
     line = line.strip()
     lineCount = lineCount + 1
-    if (line != ""):
-        curFood = curFood + int(line)
+    if line != "":
+        curElfTotalCal = curElfTotalCal + int(line)
+        curElf.snacks.append(int(line))
     else:
-        elfCount = elfCount +1
-        if (curFood > maxFood):
-            maxFood = curFood
-            print("New Max Food: " + str(maxFood))
-        curFood = 0
+        elfList.append(copy.deepcopy(curElf))
+        curElf.snacks.clear()
+        elfCount = elfCount + 1
+        if curElfTotalCal > maxCalForOneElf:
+            maxCalForOneElf = curElfTotalCal
+            print("New Max Calories: " + str(maxCalForOneElf))
+        curElfTotalCal = 0
     print(line)
 
-print ("Total of lines: " + str(lineCount))
-print ("Total of Elf: " + str(elfCount))
-print ("Elf with Max food has: " + str(maxFood) + " calories")
 f.close()
+
+
+print("Total of lines: " + str(lineCount))
+print("Total of Elfs: " + str(elfCount))
+print("Total of Elfs: " + str(len(elfList)))
+print("Elf with Max food has: " + str(maxCalForOneElf) + " calories")
+
+elfList.sort(reverse=True, key=Elf.calories)
+
+print (elfList[0].snacks)
+print (elfList[1].snacks)
+print (elfList[2].snacks)
+print (str(elfList[0].calories()))
+print (str(elfList[1].calories()))
+print (str(elfList[2].calories()))
+top3TotalCal = 0
+for elf in elfList[0:3]:
+    top3TotalCal = top3TotalCal + elf.calories()
+print ("Total calories for top 3 Elfs: " + str(top3TotalCal))
