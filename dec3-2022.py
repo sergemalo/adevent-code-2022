4e45746905a4fe1e9cd73bfa5f653b835e116705
Serge Malo <sergemalo2000@outlook.com>
2022-12-03 11:19:20 -0500

    Dec 3 - part 2 - success!

--- dec3-2022.py
+++ dec3-2022.py
@@ -28,6 +28,27 @@ class rucksack:
                     self.errorItem = l
 
 
+class ElfGroup:
+    def __init__(self):
+        self.rucksacks = []
+        self.badge = 0
+
+    def findBadge(self):
+        if (len(self.rucksacks) != 3):
+            raise ("There is not 3 Rucksacks in this group!")
+        for i0 in self.rucksacks[0].oriLine:
+            for i1 in self.rucksacks[1].oriLine:
+                for i2 in self.rucksacks[2].oriLine:
+                    if ((i0 == i1) and (i0 == i2)):
+                        print (i0)
+                        return i0
+
+        #badgeFound = False
+        #rucksack0Idx = 0
+        #while ((badgeFound == False) && (rucksack0Idx < len(rucksacks[0].oriLine))):
+        #    item0 = rucksacks[0].oriLine[0]
+
+        return "A"
 
 f = open("dec3-dataset.txt", "rt")
 lineCount = 0
@@ -35,6 +56,11 @@ lineCount = 0
 rucksackList = []
 totalPriorities = 0
 errorFoundCount = 0
+curElfGroup = ElfGroup()
+curRucksackIdxOfGroup = 0
+elfGroups = []
+badgesPriorities = 0
+
 for line in f:
     line = line.strip()
     lineCount = lineCount + 1
@@ -43,6 +69,13 @@ for line in f:
     if (curRucksack.errorFound):
         errorFoundCount = errorFoundCount + 1
         totalPriorities = totalPriorities + priority(curRucksack.errorItem)
+    curElfGroup.rucksacks.append(copy.deepcopy(curRucksack))
+    curRucksackIdxOfGroup = curRucksackIdxOfGroup + 1
+    if (curRucksackIdxOfGroup == 3):
+        badgesPriorities = badgesPriorities + priority(curElfGroup.findBadge())
+        elfGroups.append(copy.deepcopy(curElfGroup))
+        curElfGroup = ElfGroup()
+        curRucksackIdxOfGroup = 0
     rucksackList.append(copy.deepcopy(curRucksack))
 
 f.close()
@@ -52,6 +85,8 @@ print("Total of lines: " + str(lineCount))
 print("Total of Rucksacks: " + str(len(rucksackList)))
 print("Total of Errors: " + str(errorFoundCount))
 print("Total of Priorities: " + str(totalPriorities))
+print("Total of Groups: " + str(len(elfGroups)))
+print("Total of Priorities of badges: " + str(badgesPriorities))
 print ("Priority of a: " + str(priority("a")))
 print ("Priority of z: " + str(priority("z")))
 print ("Priority of A: " + str(priority("A")))
