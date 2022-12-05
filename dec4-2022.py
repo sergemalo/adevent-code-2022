8b0c4322fb40264e8556d67ae0b76961aee454a2
Serge Malo <sergemalo2000@outlook.com>
2022-12-05 05:57:01 -0500

    Dec 4 Part 2 success!

--- dec4-2022.py
+++ dec4-2022.py
@@ -11,10 +11,18 @@ class SectionRange:
             return True
         return False
 
+    def overlaps(self, otherSR):
+        if (self.endIdx < otherSR.startIdx):
+            return False
+        if (self.startIdx > otherSR.endIdx):
+            return False
+        return True
+
 
 f = open("dec4-dataset.txt", "rt")
 lineCount = 0
 sectionRangeContained = 0
+overlappingPairs = 0
 for line in f:
     line = line.strip()
     lineCount = lineCount + 1
@@ -24,13 +32,16 @@ for line in f:
     if result:
         sr1 = SectionRange(int(result.group(1)), int(result.group(2)))
         sr2 = SectionRange(int(result.group(3)), int(result.group(4)))
-        if (sr1.contains(sr2) or (sr2.contains(sr1))):
-            #print("Result: " + str(result.group(1)))
-            #print("Result: " + str(result.group(2)))
-            #print("Result: " + str(result.group(3)))
-            #print("Result: " + str(result.group(4)))
+        if sr1.contains(sr2) or (sr2.contains(sr1)):
             sectionRangeContained = sectionRangeContained + 1
-            print("Total Section Ranged contained: " + str(sectionRangeContained))
+            # print("Total Section Ranged contained: " + str(sectionRangeContained))
+        if sr1.overlaps(sr2):
+            print("Result: " + str(result.group(1)))
+            print("Result: " + str(result.group(2)))
+            print("Result: " + str(result.group(3)))
+            print("Result: " + str(result.group(4)))
+            overlappingPairs = overlappingPairs + 1
+            print("Total of Paris overlapping: " + str(overlappingPairs))
     else:
         raise ("Unable to match pattern in line: " + line)
 
@@ -39,3 +50,4 @@ f.close()
 
 print("Total of lines: " + str(lineCount))
 print("Total Section Ranged contained: " + str(sectionRangeContained))
+print("Total of Paris overlapping: " + str(overlappingPairs))
