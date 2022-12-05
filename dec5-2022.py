6368ccbe9d27f7bd40a69cd651a57029e1022600
Serge Malo <serge.malo@orolia.com>
2022-12-05 12:51:05 -0500

    Continue of Dec 5

--- dec5-2022.py
+++ dec5-2022.py
@@ -16,6 +16,17 @@ lineCount = 0
 
 parsingInitStockDone = False
 
+allStacks = {}
+allStacks[1] = ["N", "D", "M", "Q", "B", "P", "Z"]
+allStacks[2] = ["C", "L", "Z", "Q", "M", "D", "H", "V"]
+allStacks[3] = ["Q", "H", "R", "D", "V", "F", "Z", "G"]
+allStacks[4] = ["H", "G", "D", "F", "N"]
+allStacks[5] = ["N", "F", "D"]
+allStacks[6] = ["D", "Q", "V", "Z", "F", "B", "T"]
+allStacks[7] = ["Q", "M", "T", "Z", "D", "V", "S", "H"]
+allStacks[8] = ["M", "G", "F", "P", "N", "Q"]
+allStacks[9] = ["B", "W", "R", "M"]
+
 for line in f:
     line = line.strip()
     lineCount = lineCount + 1
@@ -23,16 +34,28 @@ for line in f:
     if ((len(line) > 1) and (line[0] == "1")):
         print ("Parsing Init Stock done!")
         parsingInitStockDone = True
-    if ((len(line) > 0) and parsingInitStockDone):
+    elif ((len(line) > 0) and parsingInitStockDone):
         #move 3 from 2 to 5
         pattern = "^move (\d+) from (\d+) to (\d+)$"
         result = re.match(pattern, line)
         if (result):
             print ("A" + result.group(1))
+            src = int(result.group(2))
+            dst = int(result.group(3))
+            loops = int(result.group(1))
+            for x in range(1, loops + 1):
+                allStacks[dst].append(allStacks[src].pop())
         else:
             print ("CACA")
-            raise ("Unable to parse: " + line)
+            raise Exception("Unable to parse: " + line)
 
 f.close()
 
 print("Total of lines: " + str(lineCount))
+finalStrig = ""
+for k in allStacks.keys():
+    print ("Key: " + str(k))
+    finalStrig += (allStacks[k][len(allStacks[k]) - 1])
+    print ("Stack top: " + allStacks[k][len(allStacks[k]) - 1])
+    
+print ("Final string: " + finalStrig)
